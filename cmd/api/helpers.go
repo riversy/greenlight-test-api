583243cb12a5549113015eb9327a1662ebb0b4c3
Igor Goltsov <riversy@gmail.com>
2021-11-17 00:25:49 +0300

    Add an envelop for data responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,6 +9,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]interface{}
+
 // Retrieve the "id" URL parameter from the current request context, then convert it to
 // an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
