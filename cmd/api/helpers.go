780e64182bb14495e08a8e29b4cdad058c988a1a
Igor Goltsov <riversy@gmail.com>
2021-12-05 00:38:57 +0300

    Add a Custom Runtine error to support human readable values for the int type

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -71,7 +71,7 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 			return err
 		}
 	}
-	
+
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
 		return errors.New("body must only contain a single JSON value")
